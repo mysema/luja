8949a0ae5b07c27a0c230bc833b6de041f28108e
Lassi Immonen <lassi.immonen@mysema.com>
2011-01-27 10:27:47 +0200

    Removed unncessary values

--- src/main/java/com/mysema/luja/annotations/Field.java
+++ src/main/java/com/mysema/luja/annotations/Field.java
@@ -6,16 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.apache.lucene.document.Field.Index;
-import org.apache.lucene.document.Field.Store;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Field {
 
-    String name() default "";
-    
-    Store store() default Store.YES;
-    
     Index index() default Index.ANALYZED;
     
 }
